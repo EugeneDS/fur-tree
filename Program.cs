38e7c5e9830828e7bb852ccde472db54798a442a
EugeneDS <51243209+EugeneDS@users.noreply.github.com>
2019-09-22 20:53:55 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,15 +1,35 @@
-﻿    using System;
-class Program
+﻿using System;
+
+namespace Dreamland
 {
-    public Program()
+    class Program
     {
-        void WriteHello(string someName)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello " + someName);
+            Console.WriteLine();
+            int width = int.Parse(Console.ReadLine());
+            for (int b = width; b > 0; b--)
+            {
+                for (int a = width - b; a<width; a++)
+                {
+                    Console.Write(" ");
+                }
+                for (int a = 0; a < width - b; a++)
+                {
+                    Console.Write("*");
+                }
+                for (int a = 0; a < width - b; a++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            for (int b = width-1; b > 0; b--)
+            {
+                Console.Write(" ");
+                Console.Write("*");
+            }
+            Console.ReadKey();
         }
-        WriteHello("Jo");
-        WriteHello("Don");
-        WriteHello("Peter");
-        WriteHello("You");
-    } 
+    }
 }
